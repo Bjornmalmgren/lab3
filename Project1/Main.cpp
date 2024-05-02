e3ba2ac1836ed96c56c2ff0f9e03d001f4c0a05b
Monicaco1988 <edmin.aguilaroddershede@gmail.com>
2024-05-02 15:22:18 +0200

    search parent implemented

--- Project1/Main.cpp
+++ Project1/Main.cpp
@@ -2,10 +2,11 @@
 
 using namespace std;
 class BST {
-    int data;
-    BST* left, * right;
-
 public:
+    int data;
+    BST* left, * right,*parent;
+
+
     // Default constructor.
     BST();
 
@@ -16,14 +17,17 @@ public:
     BST* Insert(BST*, int);
 
     // Inorder traversal.
-    void Inorder(BST*);
+    void InOrderWalk(BST*);
 
     void ISBalanced(BST);
+
+    BST* ParentSearch(BST*, int value);
 };
 
 // Default Constructor definition.
 BST::BST()
     : data(0)
+    , parent(NULL)
     , left(NULL)
     , right(NULL)
 {
@@ -33,7 +37,8 @@ BST::BST()
 BST::BST(int value)
 {
     data = value;
-    left = right = NULL;
+    left = right = parent= NULL;
+
 }
 
 // Insert function definition.
@@ -52,6 +57,8 @@ BST* BST::Insert(BST* root, int value)
 
         // Process right nodes.
         root->right = Insert(root->right, value);
+        if (root->right->parent != root)
+        root->right->parent = root;
     }
     else if (value < root->data) {
         // Insert left node data, if the 'value'
@@ -59,6 +66,8 @@ BST* BST::Insert(BST* root, int value)
 
         // Process left nodes.
         root->left = Insert(root->left, value);
+        if(root->left->parent != root)
+        root->left->parent = root;
     }
 
     // Return 'root' node, after insertion.
@@ -67,16 +76,50 @@ BST* BST::Insert(BST* root, int value)
 
 // Inorder traversal function.
 // This gives data in sorted order.
-void BST::Inorder(BST* root)
+void BST::InOrderWalk(BST* root)
 {
     if (!root) {
         return;
     }
-    Inorder(root->left);
+    InOrderWalk(root->left);
     cout << root->data << " ";
-    Inorder(root->right);
+    InOrderWalk(root->right);
 }
 
+
+//search the node of the parent from the value of the child
+BST* BST::ParentSearch(BST *root, int value)
+{
+    
+    if (root->data == value)
+    {
+        if (!root->parent)
+        {
+            return root;
+        }
+        return root->parent;
+    }
+    
+    if (value > root->data) {
+        // Insert right node data, if the 'value'
+        // to be inserted is greater than 'root' node data.
+
+        // Process right nodes.
+        root->right = ParentSearch(root->right, value);
+       
+    }
+    else if (value < root->data) {
+       
+        root->left = ParentSearch(root->left, value);
+        
+    }
+
+   
+}
+
+
+
+//this is left to implement...
 void BST::ISBalanced(BST root) {
     BST tootTemp = root;
 
@@ -94,7 +137,13 @@ int main(void)
     b.Insert(root, 70);
     b.Insert(root, 80);
     b.Insert(root, 60);
+    b.Insert(root, -60);
+    b.Insert(root, 120);
 
-    b.Inorder(root);
+
+
+    b.InOrderWalk(root);
+   
+
     return 0;
 }
